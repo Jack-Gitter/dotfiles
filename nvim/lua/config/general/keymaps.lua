f95fead81ea07f626e1f4e78412231f3330e41dd
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-24 14:23:04 -0500

    merging

--- nvim/lua/config/general/keymaps.lua
+++ nvim/lua/config/general/keymaps.lua
@@ -2,5 +2,11 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<space>", "<nop>", { silent = true })
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<c-p>", "<c-^>")
-vim.keymap.set("n", "<leader>vs", function() vim.cmd("split") end)
-vim.keymap.set("n", "<leader>hs", function() vim.cmd("vs") end)
+vim.keymap.set("n", "<leader>vs", function() vim.cmd("vs") end)
+vim.keymap.set("n", "<leader>s", function() vim.cmd("split") end)
+vim.keymap.set('n', '<leader>x', function()
+    local success, _ = pcall(vim.cmd, 'wq')
+    if not success then
+        vim.cmd('q!')
+    end
+end, { noremap = true, silent = true })
