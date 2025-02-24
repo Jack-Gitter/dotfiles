ee5ec91207bd61b7e5fa3b4a683e635502895c4a
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-24 14:28:10 -0500

    nice

--- nvim/lua/config/general/keymaps.lua
+++ nvim/lua/config/general/keymaps.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<space>", "<nop>", { silent = true })
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<c-p>", "<c-^>")
-vim.keymap.set("n", "<leader>vs", function() vim.cmd("vs") end)
 vim.keymap.set("n", "<leader>s", function() vim.cmd("split") end)
+vim.keymap.set("n", "<leader>v", function() vim.cmd("vs") end)
 vim.keymap.set('n', '<leader>x', function()
     local success, _ = pcall(vim.cmd, 'wq')
     if not success then
