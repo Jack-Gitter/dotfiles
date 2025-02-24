dff8c600eeb037747504ae6a6d049c22931f1c20
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-24 12:50:06 -0500

    nie

--- nvim/lua/config/general/keymaps.lua
+++ nvim/lua/config/general/keymaps.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<space>", "<nop>", { silent = true })
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<c-p>", "<c-^>")
-
-
+vim.keymap.set("n", "<c-w>v", function() vim.cmd("split") end)
+vim.keymap.set("n", "<c-w>h", function() vim.cmd("vs") end)
