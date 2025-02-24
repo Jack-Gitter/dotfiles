a2ada928e249d5ae8dec7b8e1503ddd4ae7c0acb
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-24 11:40:54 -0500

    ok

--- install/nvim/install.bash
+++ install/nvim/install.bash
@@ -5,6 +5,11 @@ if ! command -v "brew" >/dev/null 2>&1; then
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 fi
 
+if ! command -v fzf >/dev/null 2>&1; then 
+    echo "installing fzf"
+    brew install fzf
+fi
+
 if ! command -v "nvim" >/dev/null 2>&1; then
     echo "installing neovim..."
     brew install neovim
