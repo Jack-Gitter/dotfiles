6d9014b7a1e7f866ad33621b156ff71dc8919af1
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-25 08:47:05 -0500

    nice

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -5,12 +5,12 @@ return {
     },
     config = function()
         local luasnip = require("luasnip")
+        require("luasnip").config.setup({ store_selection_keys = "<Tab>" })
+        require("luasnip.loaders.from_snipmate").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_lua").load({ paths = "./snippets" })
         vim.keymap.set({ "i", "s" }, "<C-l>", function() luasnip.jump(1) end, { silent = true })
         vim.keymap.set({ "i", "s" }, "<C-h>", function() luasnip.jump(-1) end, { silent = true })
-        require("luasnip").config.setup({ store_selection_keys = "<Tab>" })
-        require("luasnip.loaders.from_vscode").lazy_load()
-        require("luasnip.loaders.from_snipmate").lazy_load()
-        require("luasnip.loaders.from_lua").load({ paths = "./snippets" })
     end,
     event = { "BufReadPost", "BufNewFile" },
 }
