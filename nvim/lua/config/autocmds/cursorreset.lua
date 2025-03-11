64c59c4ef926aff1b6c9c6d5fb46676ce71921e2
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-03-11 08:07:45 -0400

    working

--- nvim/lua/config/autocmds/cursorreset.lua
+++ nvim/lua/config/autocmds/cursorreset.lua
@@ -9,5 +9,5 @@ local restore_cursor_augroup = vim.api.nvim_create_augroup(
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
     group = restore_cursor_augroup,
     desc = "restore the cursor shape on exit of neovim",
-    command = "set guicursor=a:ver20",
+    command = "set guicursor=a:hor20",
 })
