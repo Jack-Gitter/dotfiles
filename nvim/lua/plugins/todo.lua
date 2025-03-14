0cfe243f3415531c433ce9eb233725e61b7c7243
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-03-14 09:35:35 -0400

    ok

--- nvim/lua/plugins/todo.lua
+++ nvim/lua/plugins/todo.lua
@@ -1,4 +1,15 @@
 return {
     'Jack-Gitter/todo.nvim',
+    dependencies = {
+        {
+            'MeanderingProgrammer/render-markdown.nvim',
+            dependencies = { 'nvim-treesitter/nvim-treesitter' },
+            config = function()
+                require('render-markdown').setup({
+                    completions = { lsp = { enabled = true } },
+                })
+            end
+        }
+    },
     opts = {}
 }
