5fba8494cfd03abc2e009455a2b5facd4a6ad830
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-03-14 09:09:09 -0400

    ok

--- nvim/lua/localplugins/todo/lua/todo.lua
+++ nvim/lua/localplugins/todo/lua/todo.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local opts = {
-    fullpath = vim.fn.stdpath("data") .. 'lazy/todo/todo.md',
+    fullpath = vim.env.XDG_CONFIG_HOME .. '/nvim/lua/localplugins/todo/todo.md',
     keymap = '<leader>td',
     win_opts = {
         relative = 'editor',
