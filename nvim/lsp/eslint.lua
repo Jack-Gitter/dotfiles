3031a6703a4d05543421eb23e90b471ad0bb957f
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-04-04 14:00:46 -0400

    ok

--- nvim/lsp/eslint.lua
+++ nvim/lsp/eslint.lua
@@ -64,85 +64,85 @@ local root_markers = {
     'eslint.config.cts',
 }
 
-return {
-    cmd = { 'vscode-eslint-language-server', '--stdio' },
-    filetypes = {
-        'javascript',
-        'javascriptreact',
-        'javascript.jsx',
-        'typescript',
-        'typescriptreact',
-        'typescript.tsx',
-        'vue',
-        'svelte',
-        'astro',
-    },
-    commands = {
-        EslintFixAll = {
-            function()
-                fix_all { sync = true, bufnr = 0 }
-            end,
-            description = 'Fix all eslint problems for this buffer',
-        },
-    },
-    root_markers = root_markers,
-    on_attach = function(_, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-        })
-    end,
-    settings = {
-        validate = 'on',
-        packageManager = nil,
-        useESLintClass = false,
-        experimental = {
-            useFlatConfig = false,
-        },
-        codeActionOnSave = {
-            enable = false,
-            mode = 'all',
-        },
-        format = true,
-        quiet = false,
-        onIgnoredFiles = 'off',
-        rulesCustomizations = {},
-        run = 'onType',
-        problems = {
-            shortenToSingleLine = false,
-        },
-        nodePath = '',
-        workingDirectory = { mode = 'location' },
-        codeAction = {
-            disableRuleComment = {
-                enable = true,
-                location = 'separateLine',
-            },
-            showDocumentation = {
-                enable = true,
-            },
-        },
-    },
-    handlers = {
-        ['eslint/openDoc'] = function(_, result)
-            if result then
-                vim.ui.open(result.url)
-            end
-            return {}
-        end,
-        ['eslint/confirmESLintExecution'] = function(_, result)
-            if not result then
-                return
-            end
-            return 4
-        end,
-        ['eslint/probeFailed'] = function()
-            vim.notify('[lspconfig] ESLint probe failed.', vim.log.levels.WARN)
-            return {}
-        end,
-        ['eslint/noLibrary'] = function()
-            vim.notify('[lspconfig] Unable to find ESLint library.', vim.log.levels.WARN)
-            return {}
-        end,
-    },
-}
+-- return {
+--     cmd = { 'vscode-eslint-language-server', '--stdio' },
+--     filetypes = {
+--         'javascript',
+--         'javascriptreact',
+--         'javascript.jsx',
+--         'typescript',
+--         'typescriptreact',
+--         'typescript.tsx',
+--         'vue',
+--         'svelte',
+--         'astro',
+--     },
+--     commands = {
+--         EslintFixAll = {
+--             function()
+--                 fix_all { sync = true, bufnr = 0 }
+--             end,
+--             description = 'Fix all eslint problems for this buffer',
+--         },
+--     },
+--     root_markers = root_markers,
+--     -- on_attach = function(_, bufnr)
+--     --     vim.api.nvim_create_autocmd("BufWritePre", {
+--     --         buffer = bufnr,
+--     --         command = "EslintFixAll",
+--     --     })
+--     -- end,
+--     settings = {
+--         validate = 'on',
+--         packageManager = nil,
+--         useESLintClass = false,
+--         experimental = {
+--             useFlatConfig = false,
+--         },
+--         codeActionOnSave = {
+--             enable = false,
+--             mode = 'all',
+--         },
+--         format = true,
+--         quiet = false,
+--         onIgnoredFiles = 'off',
+--         rulesCustomizations = {},
+--         run = 'onType',
+--         problems = {
+--             shortenToSingleLine = false,
+--         },
+--         nodePath = '',
+--         workingDirectory = { mode = 'location' },
+--         codeAction = {
+--             disableRuleComment = {
+--                 enable = true,
+--                 location = 'separateLine',
+--             },
+--             showDocumentation = {
+--                 enable = true,
+--             },
+--         },
+--     },
+--     handlers = {
+--         ['eslint/openDoc'] = function(_, result)
+--             if result then
+--                 vim.ui.open(result.url)
+--             end
+--             return {}
+--         end,
+--         ['eslint/confirmESLintExecution'] = function(_, result)
+--             if not result then
+--                 return
+--             end
+--             return 4
+--         end,
+--         ['eslint/probeFailed'] = function()
+--             vim.notify('[lspconfig] ESLint probe failed.', vim.log.levels.WARN)
+--             return {}
+--         end,
+--         ['eslint/noLibrary'] = function()
+--             vim.notify('[lspconfig] Unable to find ESLint library.', vim.log.levels.WARN)
+--             return {}
+--         end,
+--     },
+-- }
