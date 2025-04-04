6284feb70fcd21d160ed81c2334b97991d0b8758
Jack Gitter <jack.gitter@gmail.com>
2025-04-04 17:10:55 -0400

    added

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -19,7 +19,7 @@ return {
     },
     config = function()
         require("mason").setup()
-        require("mason-lspconfig").setup({ ensure_installed = { "gopls", "pyright", "eslint", } })
+        require("mason-lspconfig").setup({ ensure_installed = { "ts_ls", "lua_ls", "gopls", "pyright", "eslint", } })
         require("mason-nvim-dap").setup({ ensure_installed = { "js", "delve" } })
 
         vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { bg = '#5a4480', fg = 'white' })
