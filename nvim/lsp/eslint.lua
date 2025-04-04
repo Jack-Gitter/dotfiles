831898188bc0271a3e30ceaf1de862f61a16db01
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-04-04 14:32:58 -0400

    nice

--- nvim/lsp/eslint.lua
+++ nvim/lsp/eslint.lua
@@ -1,11 +1,10 @@
-local utils = require("utils.utils")
-
+local util = require 'lspconfig.util'
 local lsp = vim.lsp
 
-local fix_all = function(opts)
+local function fix_all(opts)
     opts = opts or {}
 
-    local eslint_lsp_client = utils.get_active_client_by_name(opts.bufnr, 'eslint')
+    local eslint_lsp_client = util.get_active_client_by_name(opts.bufnr, 'eslint')
     if eslint_lsp_client == nil then
         return
     end
@@ -21,7 +20,7 @@ local fix_all = function(opts)
         end
     end
 
-    local bufnr = utils.validate_bufnr(opts.bufnr or 0)
+    local bufnr = util.validate_bufnr(opts.bufnr or 0)
     request(0, 'workspace/executeCommand', {
         command = 'eslint.applyAllFixes',
         arguments = {
@@ -33,23 +32,7 @@ local fix_all = function(opts)
     })
 end
 
-local function get_eslint_closest_dir()
-    local cwd = vim.fn.getcwd()
-    local eslint_node_modules = vim.fn.finddir('node_modules/eslint', cwd .. ';')
-
-    if eslint_node_modules == '' then
-        return nil
-    end
-
-    if eslint_node_modules == 'node_modules/eslint' then
-        return cwd
-    end
-
-    -- Strip the node_modules/eslint part
-    return eslint_node_modules:match '(.*)/node_modules/eslint'
-end
-
-local root_markers = {
+local root_file = {
     '.eslintrc',
     '.eslintrc.js',
     '.eslintrc.cjs',
@@ -64,6 +47,21 @@ local root_markers = {
     'eslint.config.cts',
 }
 
+local function get_eslint_closest_dir()
+    local cwd = vim.fn.getcwd()
+    local eslint_node_modules = vim.fn.finddir('node_modules/eslint', cwd .. ';')
+
+    if eslint_node_modules == '' then
+        return nil
+    end
+
+    if eslint_node_modules == 'node_modules/eslint' then
+        return cwd
+    end
+
+    return eslint_node_modules:match '(.*)/node_modules/eslint'
+end
+
 return {
     cmd = { 'vscode-eslint-language-server', '--stdio' },
     filetypes = {
@@ -77,7 +75,82 @@ return {
         'svelte',
         'astro',
     },
-    root_markers = root_markers,
+    root_markers = root_file,
+    settings = {
+        validate = 'on',
+        packageManager = nil,
+        useESLintClass = false,
+        experimental = {
+            useFlatConfig = false,
+        },
+        codeActionOnSave = {
+            enable = false,
+            mode = 'all',
+        },
+        format = true,
+        quiet = false,
+        onIgnoredFiles = 'off',
+        rulesCustomizations = {},
+        run = 'onType',
+        problems = {
+            shortenToSingleLine = false,
+        },
+        nodePath = '',
+        workingDirectory = { mode = 'location' },
+        codeAction = {
+            disableRuleComment = {
+                enable = true,
+                location = 'separateLine',
+            },
+            showDocumentation = {
+                enable = true,
+            },
+        },
+    },
+    on_attach = function(config, new_root_dir)
+        new_root_dir = get_eslint_closest_dir() or new_root_dir
+        config.settings.workspaceFolder = {
+            uri = new_root_dir,
+            name = vim.fn.fnamemodify(new_root_dir, ':t'),
+        }
+        if
+            vim.fn.filereadable(new_root_dir .. '/eslint.config.js') == 1
+            or vim.fn.filereadable(new_root_dir .. '/eslint.config.mjs') == 1
+            or vim.fn.filereadable(new_root_dir .. '/eslint.config.cjs') == 1
+            or vim.fn.filereadable(new_root_dir .. '/eslint.config.ts') == 1
+            or vim.fn.filereadable(new_root_dir .. '/eslint.config.mts') == 1
+            or vim.fn.filereadable(new_root_dir .. '/eslint.config.cts') == 1
+        then
+            config.settings.experimental.useFlatConfig = true
+        end
+        local pnp_cjs = new_root_dir .. '/.pnp.cjs'
+        local pnp_js = new_root_dir .. '/.pnp.js'
+        if vim.loop.fs_stat(pnp_cjs) or vim.loop.fs_stat(pnp_js) then
+            config.cmd = vim.list_extend({ 'yarn', 'exec' }, config.cmd)
+        end
+    end,
+    handlers = {
+        ['eslint/openDoc'] = function(_, result)
+            if result then
+                vim.ui.open(result.url)
+            end
+            return {}
+        end,
+        ['eslint/confirmESLintExecution'] = function(_, result)
+            if not result then
+                return
+            end
+            return 4 -- approved
+        end,
+        ['eslint/probeFailed'] = function()
+            vim.notify('[lspconfig] ESLint probe failed.', vim.log.levels.WARN)
+            return {}
+        end,
+        ['eslint/noLibrary'] = function()
+            vim.notify('[lspconfig] Unable to find ESLint library.', vim.log.levels.WARN)
+            return {}
+        end,
+    },
     commands = {
         EslintFixAll = {
             function()
