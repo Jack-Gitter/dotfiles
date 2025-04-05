83b9d2a2101223275c8f9b7977eece0b0c402cc4
Jack Gitter <jack.gitter@gmail.com>
2025-04-04 20:36:07 -0400

    nice

--- nvim/lsp2/gopls.lua
+++ nvim/lsp2/gopls.lua
@@ -1,22 +1,11 @@
 return {
     cmd = { 'gopls' },
     filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-    root_dir = function(fname)
-      if not mod_cache then
-        local result = async.run_command { 'go', 'env', 'GOMODCACHE' }
-        if result and result[1] then
-          mod_cache = vim.trim(result[1])
-        else
-          mod_cache = vim.fn.system 'go env GOMODCACHE'
-        end
-      end
-      if mod_cache and fname:sub(1, #mod_cache) == mod_cache then
-        local clients = util.get_lsp_clients { name = 'gopls' }
-        if #clients > 0 then
-          return clients[#clients].config.root_dir
-        end
-      end
-      return util.root_pattern('go.work', 'go.mod', '.git')(fname)
-    end,
+    root_markers = { 'go.work', 'go.mod', '.git' }
     single_file_support = true,
+    settings = {
+        gopls = {
+            symbolScope = "workspace",
+        }
+    },
 }
