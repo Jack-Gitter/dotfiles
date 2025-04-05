8c2c99d5e3bd74ccfa0d8d3536ed32ee45b4658d
Jack Gitter <jack.gitter@gmail.com>
2025-04-04 21:43:52 -0400

    nice

--- nvim/lua/config/general/settings.lua
+++ nvim/lua/config/general/settings.lua
@@ -12,7 +12,3 @@ vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.cursorline = true
 vim.opt.cursorlineopt = "number"
-
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#baa0e8", })
-vim.api.nvim_set_hl(0, 'ColorColumn', { fg = '#a787de' })
-vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { bg = '#5a4480', fg = 'white' })
