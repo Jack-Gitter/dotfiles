3f56750dd9f2b712d0edf41e31e8d401a2f5e97c
Jack Gitter <jack.gitter@gmail.com>
2025-04-04 21:43:42 -0400

    nice

---
+++ nvim/lua/config/general/highlights.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#baa0e8", })
+vim.api.nvim_set_hl(0, 'ColorColumn', { fg = '#a787de' })
+vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { bg = '#5a4480', fg = 'white' })
--- nvim/lua/config/general/init.lua
+++ nvim/lua/config/general/init.lua
@@ -1,2 +1,3 @@
 require("config.general.keymaps")
 require("config.general.settings")
+require("config.general.highlights")
