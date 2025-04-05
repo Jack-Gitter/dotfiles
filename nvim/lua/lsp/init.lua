15f707e4055b813f1eff2f178b042127207d553b
Jack Gitter <jack.gitter@gmail.com>
2025-04-05 17:41:24 -0400

    nice

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,7 +1,7 @@
 require("lsp.eslint")
- require("lsp.ts_ls")
- require("lsp.gopls")
- require("lsp.pyright")
- require("lsp.lua_ls")
+require("lsp.ts_ls")
+require("lsp.gopls")
+require("lsp.pyright")
+require("lsp.lua_ls")
 
 vim.lsp.enable({ "ts_ls", "eslint", "lua_ls", "pyright", "gopls" })
