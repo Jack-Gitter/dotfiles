07b9a6e712c2537978539659f0a2010dc5f711de
Jack Gitter <jack.gitter@gmail.com>
2025-04-04 20:47:46 -0400

    ok

--- nvim/lua/plugins/lspmanager.lua
+++ nvim/lua/plugins/lspmanager.lua
@@ -1,26 +1,16 @@
 return {
-    "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
     dependencies = {
-        {
-            "williamboman/mason.nvim",
-        },
-        {
-            'saghen/blink.cmp'
-        },
         {
             "williamboman/mason-lspconfig.nvim"
         },
         {
             "jay-babu/mason-nvim-dap.nvim"
         },
-        {
-            "artemave/workspace-diagnostics.nvim"
-        }
     },
     config = function()
         require("mason").setup()
         require("mason-lspconfig").setup({ ensure_installed = { "ts_ls", "lua_ls", "gopls", "pyright", "eslint", } })
         require("mason-nvim-dap").setup({ ensure_installed = { "js", "delve" } })
     end,
-    event = { "BufReadPost", "BufNewFile" },
 }
