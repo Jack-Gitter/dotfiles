f863f118d9731a100e13721eda12417050d7211e
Jack Gitter <jack.gitter@gmail.com>
2025-04-04 20:45:26 -0400

    nice

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -23,59 +23,6 @@ return {
         require("mason-nvim-dap").setup({ ensure_installed = { "js", "delve" } })
 
         vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { bg = '#5a4480', fg = 'white' })
-
-        local lspconfig = require("lspconfig")
-        local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-        local on_attach = function(client, bufnr)
-            local ft = vim.bo.filetype
-
-            if client.supports_method("textDocument/hover") then
-                vim.keymap.set("n", "<leader>ho", function() vim.lsp.buf.hover({ border = "single" }) end,
-                    { buffer = true })
-            end
-
-            if client.supports_method("textDocument/diagnostics") then
-                vim.keymap.set("n", "<leader>di", vim.diagnostic.open_float)
-            end
-
-            if ft == "javascript" or ft == "typescript" then
-                require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
-            end
-
-
-            if client.supports_method("textDocument/formatting") and ft ~= "python" and ft ~= "javascript" and ft ~= "typescript" then
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format()
-                        if vim.bo.filetype == "lua" then
-                            vim.diagnostic.enable()
-                        end
-                    end
-                })
-            end
-
-            if client.supports_method("textDocument/signatureHelp") then
-                vim.keymap.set("i", "<c-;>", function() vim.lsp.buf.signature_help({ border = "single", title = "" }) end)
-            end
-
-
-            if client.supports_method("textDocument/rename") then
-                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-            end
-        end
-
-
-        --     lspconfig.gopls.setup({
-        --         on_attach = on_attach,
-        --         capabilities = capabilities,
-        --         settings = {
-        --             gopls = {
-        --                 symbolScope = "workspace",
-        --             }
-        --         },
-        --     })
-    end,
+    end
     event = { "BufReadPost", "BufNewFile" },
 }
