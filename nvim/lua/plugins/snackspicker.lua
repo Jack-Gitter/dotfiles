74a0b7f4707b799334559e3edcadb5b2d2fa8521
Jack Gitter <jack.gitter@gmail.com>
2025-04-05 10:02:29 -0400

    moving there, really need a code actions picker

--- nvim/lua/plugins/snackspicker.lua
+++ nvim/lua/plugins/snackspicker.lua
@@ -2,6 +2,28 @@ return {
     "folke/snacks.nvim",
     opts = {
         picker = {
+        },
+        list = {
+            keys = {
+                ["<c-h>"] = "toggle_hidden",
+                ["<c-i>"] = "toggle_ignored",
+            }
         }
-    }
+    },
+    config = function(_, _)
+        local ws = function()
+            local folders = vim.lsp.buf.list_workspace_folders()
+            if next(folders) ~= nil then
+                return folders[1]
+            else
+                return vim.fn.getcwd()
+            end
+        end
+        vim.keymap.set("n", "<leader>ff", function() Snacks.picker.smart({ cwd = ws() }) end)
+        vim.keymap.set("n", "<leader>gr", function() Snacks.picker.grep({ cwd = ws() }) end)
+        vim.keymap.set("n", "<leader>fr", function() Snacks.picker.lsp_references() end)
+        vim.keymap.set("n", "<leader>gd", function() Snacks.picker.lsp_definitions() end)
+        vim.keymap.set("n", "<leader>ws", function() Snacks.picker.lsp_symbols() end)
+        vim.keymap.set("n", "<leader>wd", function() Snacks.picker.diagnostics() end)
+    end
 }
