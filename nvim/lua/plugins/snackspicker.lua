4bd986af1f94a04933bc4111515a295974d396d1
Jack Gitter <jack.gitter@gmail.com>
2025-04-05 18:08:00 -0400

    nice

--- nvim/lua/plugins/snackspicker.lua
+++ nvim/lua/plugins/snackspicker.lua
@@ -1,24 +1,24 @@
-local ws = function()
-    local folders = vim.lsp.buf.list_workspace_folders()
-    if next(folders) ~= nil then
-        return folders[1]
-    else
-        return vim.fn.getcwd()
-    end
-end
-
 return {
     "folke/snacks.nvim",
     opts = {
         picker = {}
     },
-    keys = {
-        { "<leader>ff", function() Snacks.picker.smart({ cwd = ws() }) end },
-        { "<leader>gr", function() Snacks.picker.grep({ cwd = ws() }) end },
-        { "<leader>fr", function() Snacks.picker.lsp_references() end },
-        { "<leader>gi", function() Snacks.picker.lsp_implementations() end },
-        { "<leader>gd", function() Snacks.picker.lsp_definitions() end },
-        { "<leader>ws", function() Snacks.picker.lsp_workspace_symbols() end },
-        { "<leader>wd", function() Snacks.picker.diagnostics() end }
-    }
+    config = function(_, opts)
+        local ws = function()
+            local folders = vim.lsp.buf.list_workspace_folders()
+            if next(folders) ~= nil then
+                return folders[1]
+            else
+                return vim.fn.getcwd()
+            end
+        end
+
+        vim.keymap.set("n", "<leader>ff", function() Snacks.picker.smart({ cwd = ws() }) end)
+        vim.keymap.set("<leader>gr", function() Snacks.picker.grep({ cwd = ws() }) end)
+        vim.keymap.set("n", "<leader>fr", Snacks.picker.lsp_references)
+        vim.keymap.set("n", "<leader>gi", Snacks.picker.lsp_implementations)
+        vim.keymap.set("n", "<leader>gd", Snacks.picker.lsp_definitions)
+        vim.keymap.set("n", "<leader>ws", Snacks.picker.lsp_workspace_symbols)
+        vim.keymap.set("n", "<leader>wd", Snacks.picker.diagnostics)
+    end
 }
