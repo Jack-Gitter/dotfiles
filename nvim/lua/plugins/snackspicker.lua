27459cb6dcedd81d184770bf5ff6ddbd608fe17d
Jack Gitter <jack.gitter@gmail.com>
2025-04-05 17:52:53 -0400

    nice

--- nvim/lua/plugins/snackspicker.lua
+++ nvim/lua/plugins/snackspicker.lua
@@ -1,26 +1,22 @@
 return {
-    "folke/snacks.nvim",
-    opts = {
-        ui_select = true
-    },
-    config = function(_, opts)
-        local snacks = require("snacks")
-        snacks.setup(opts)
+	"folke/snacks.nvim",
+	config = function()
+		local snacks = require("snacks")
 
-        local ws = function()
-            local folders = vim.lsp.buf.list_workspace_folders()
-            if next(folders) ~= nil then
-                return folders[1]
-            else
-                return vim.fn.getcwd()
-            end
-        end
-        vim.keymap.set("n", "<leader>ff", function() snacks.picker.smart({ cwd = ws() }) end)
-        vim.keymap.set("n", "<leader>gr", function() snacks.picker.grep({ cwd = ws() }) end)
-        vim.keymap.set("n", "<leader>fr", function() snacks.picker.lsp_references() end)
-        vim.keymap.set("n", "<leader>fr", function() snacks.picker.lsp_implementations() end)
-        vim.keymap.set("n", "<leader>gd", function() snacks.picker.lsp_definitions() end)
-        vim.keymap.set("n", "<leader>ws", function() snacks.picker.lsp_workspace_symbols() end)
-        vim.keymap.set("n", "<leader>wd", function() snacks.picker.diagnostics() end)
-    end
+		local ws = function()
+			local folders = vim.lsp.buf.list_workspace_folders()
+			if next(folders) ~= nil then
+				return folders[1]
+			else
+				return vim.fn.getcwd()
+			end
+		end
+		vim.keymap.set("n", "<leader>ff", function() snacks.picker.smart({ cwd = ws() }) end)
+		vim.keymap.set("n", "<leader>gr", function() snacks.picker.grep({ cwd = ws() }) end)
+		vim.keymap.set("n", "<leader>fr", function() snacks.picker.lsp_references() end)
+		vim.keymap.set("n", "<leader>fr", function() snacks.picker.lsp_implementations() end)
+		vim.keymap.set("n", "<leader>gd", function() snacks.picker.lsp_definitions() end)
+		vim.keymap.set("n", "<leader>ws", function() snacks.picker.lsp_workspace_symbols() end)
+		vim.keymap.set("n", "<leader>wd", function() snacks.picker.diagnostics() end)
+	end
 }
