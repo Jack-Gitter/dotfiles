59ba1983bd7c7134efc22f7b951710ab11d8e1a8
Jack Gitter <jack.gitter@gmail.com>
2025-04-06 13:55:13 -0400

    nice

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -6,6 +6,6 @@ return {
     config = function()
         vim.keymap.set({ 'n', 'v' }, 'f', '<plug>(leap-forward)')
         vim.keymap.set({ 'n', 'v' }, '<s-f>', '<plug>(leap-backward)')
-    end
+    end,
     keys = {"f", "F"}
 }
