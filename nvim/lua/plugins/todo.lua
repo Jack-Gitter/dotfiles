dfb51213ce5a24246015e6a1412991ccc63823f0
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-04-11 11:29:28 -0400

    updated

--- nvim/lua/plugins/todo.lua
+++ nvim/lua/plugins/todo.lua
@@ -1,15 +1,7 @@
 return {
     'Jack-Gitter/todo.nvim',
     dependencies = {
-        {
-            'MeanderingProgrammer/render-markdown.nvim',
-            dependencies = { 'nvim-treesitter/nvim-treesitter' },
-            config = function()
-                require('render-markdown').setup({
-                    completions = { lsp = { enabled = true } },
-                })
-            end
-        }
+        'MeanderingProgrammer/render-markdown.nvim',
     },
     opts = {
         fullpath = vim.env.HOME .. '/.config/todo.md',
@@ -18,5 +10,5 @@ return {
         local todo = require('todo')
         todo.setup(opts)
     end,
-    keys = {"<leader>td"}
+    keys = { "<leader>td" }
 }
