43d8578eba473e880ab9e04c6db2e8138fa4ab17
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-04-16 15:10:49 -0400

    ok

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -3,5 +3,6 @@ require('config.lsp.ts_ls')
 require('config.lsp.gopls')
 require('config.lsp.pyright')
 require('config.lsp.lua_ls')
+require('config.lsp.kulala_ls')
 
-vim.lsp.enable({ 'ts_ls', 'eslint', 'lua_ls', 'pyright', 'gopls' })
+vim.lsp.enable({ 'ts_ls', 'eslint', 'lua_ls', 'pyright', 'gopls', 'kulala_ls' })
---
+++ nvim/lua/plugins/kulala.lua
@@ -0,0 +1,27 @@
+return {
+    'mistweaverco/kulala.nvim',
+    keys = {
+        { '<leader>rs', desc = 'Send request' },
+        { '<leader>ra', desc = 'Send all requests' },
+        { '<leader>rb', desc = 'Open scratchpad' },
+    },
+    ft = { 'http', 'rest' },
+    opts = {
+        global_keymaps = {
+            ['Send request'] = {
+                '<leader>rs',
+                function() require('kulala').run() end,
+                desc = 'Send request'
+            },
+            ['Send all requests'] = {
+                '<leader>ra',
+                function() require('kulala').run_all() end,
+            },
+            ['Replay the last request'] = {
+                '<leader>rr',
+                function() require('kulala').replay() end,
+            },
+            ['Find request'] = false
+        },
+    },
+}
