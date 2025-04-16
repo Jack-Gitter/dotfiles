d2361df0b08a0a0e3c1741b96153ca4d96b1ddbb
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-04-16 15:20:30 -0400

    treesitter working... so dumb

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,14 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     opts = {
-        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'sql', 'markdown', 'markdown_inline', 'javascript', 'typescript', 'luadoc', 'gopls' },
+        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'sql', 'markdown', 'markdown_inline', 'javascript', 'typescript', 'luadoc', 'gopls', 'http' },
+        auto_install = true,
         highlight = {
             enable = true
         }
     },
-    event = { 'BufReadPost', 'BufNewFile' },
+    config = function(_, opts)
+        require('nvim-treesitter').setup(opts)
+        vim.cmd('TSEnable highlight')
+    end
 }
