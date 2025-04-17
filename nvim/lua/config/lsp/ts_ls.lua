d4155682817344026647a4f16158a98f605b597e
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-04-17 14:02:38 -0400

    ft plugins

--- nvim/lua/config/lsp/ts_ls.lua
+++ nvim/lua/config/lsp/ts_ls.lua
@@ -22,9 +22,6 @@ local on_attach = function(client, bufnr)
     if client.supports_method('textDocument/rename') then
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
     end
-
-    vim.cmd('compiler tsc')
-    vim.o.makeprg = 'npx tsc'
 end
 
 local root_markers = {
