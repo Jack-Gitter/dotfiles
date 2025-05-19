73f2b90975c311255506c1f57bb9635dfafb2e7f
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-05-19 09:27:03 -0400

    treesitter fixed

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,13 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    opts = {
-        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'sql', 'markdown', 'markdown_inline', 'javascript', 'typescript', 'luadoc', 'gopls', 'http' },
-        auto_install = true,
-        highlight = {
-            enable = true
-        }
-    },
-    config = function(_, opts)
-        require('nvim-treesitter').setup(opts)
-        vim.cmd('TSEnable highlight')
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'sql', 'markdown', 'markdown_inline', 'javascript', 'typescript', 'luadoc', 'http' },
+            auto_install = true,
+            highlight = {
+                enable = true
+            }
+        })
     end,
     event = { 'BufEnter' }
 }
