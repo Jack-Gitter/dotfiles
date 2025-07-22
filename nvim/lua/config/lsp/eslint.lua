ec1cee8974f2ee1101b92d2872a66a8f5744a389
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-07-22 16:29:38 -0400

    better, need to fix this shit bad

--- nvim/lua/config/lsp/eslint.lua
+++ nvim/lua/config/lsp/eslint.lua
@@ -1,3 +1,32 @@
+local function fix_all(opts)
+    opts = opts or {}
+
+    local bufnr = opts.bufnr or vim.api.nvim_get_current_buf()
+    vim.validate('bufnr', bufnr, 'number')
+
+    local client = opts.client or vim.lsp.get_clients({ bufnr = bufnr, name = 'eslint' })[1]
+
+    if not client then return end
+
+    local request
+
+    if opts.sync then
+        request = function(buf, method, params) client:request_sync(method, params, nil, buf) end
+    else
+        request = function(buf, method, params) client:request(method, params, nil, buf) end
+    end
+
+    request(bufnr, 'workspace/executeCommand', {
+        command = 'eslint.applyAllFixes',
+        arguments = {
+            {
+                uri = vim.uri_from_bufnr(bufnr),
+                version = vim.lsp.util.buf_versions[bufnr],
+            },
+        },
+    })
+end
+
 local function get_eslint_closest_dir()
     local cwd = vim.fn.getcwd()
     local eslint_node_modules = vim.fn.finddir('node_modules/eslint', cwd .. ';')
@@ -15,22 +44,16 @@ end
 
 local on_attach = function(client, bufnr)
     local root_dir = client.root_dir
+    vim.api.nvim_create_user_command('EslintFixAll', function() fix_all({ client = client, sync = true }) end, {})
 
     if root_dir == nil then
         client.stop()
         return
     end
 
-    vim.api.nvim_create_autocmd('BufWritePre', {
+    vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
         pattern = { '*.js', '*.jsx', '*.ts', '*.tsx' },
-        callback = function()
-            vim.lsp.buf.format({
-                async = false,
-                filter = function(c)
-                    return c.name == 'eslint'
-                end
-            })
-        end,
+        command = 'silent! EslintFixAll',
     })
 
 
