0eca9bbe2e4f19ebad34888931f3c584082b7d55
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-07-22 15:54:12 -0400

    nice

--- nvim/lua/config/lsp/ts_ls.lua
+++ nvim/lua/config/lsp/ts_ls.lua
@@ -1,6 +1,4 @@
-local on_attach = function(client, bufnr)
-    require('workspace-diagnostics').populate_workspace_diagnostics(client, bufnr)
-
+local on_attach = function(client)
     if client.supports_method('textDocument/hover') then
         vim.keymap.set('n', '<leader>ho', function() vim.lsp.buf.hover({ border = 'single' }) end,
             { buffer = true })
