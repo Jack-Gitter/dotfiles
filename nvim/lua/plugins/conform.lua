31aa4642b66cc3b11610c37240ca078e70e5da4f
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-07-23 13:23:07 -0400

    ok

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,8 +2,7 @@ return {
     'stevearc/conform.nvim',
     opts = {
         format_on_save = {
-            timeout_ms = 500,
-            -- lsp_format = 'fallback',
+            timeout_ms = 1000,
         },
         formatters_by_ft = {
             javascript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
