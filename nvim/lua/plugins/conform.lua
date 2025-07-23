9d00269196e0e9e3b00fe4cf4f5df8b5f9d8ff28
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-07-23 09:17:50 -0400

    conform is so much better than eslint language server!

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,8 +1,12 @@
 return {
     'stevearc/conform.nvim',
     opts = {
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+        },
         formatters_by_ft = {
-            javascript = { 'prettierd', 'prettier', stop_after_first = true },
+            javascript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
         },
     },
 }
