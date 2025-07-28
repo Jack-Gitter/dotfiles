7e2959f592f669ca61fde4bfdbf1214eba3c01a0
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-07-28 08:32:40 -0400

    bad map

--- nvim/lua/config/general/keymaps.lua
+++ nvim/lua/config/general/keymaps.lua
@@ -16,5 +16,5 @@ end, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>hl', ':nohl<cr>')
 
 vim.keymap.set('n', '<leader>L', function() vim.cmd('vertical resize -15') end)
-vim.keymap.set('n', '<leader>J', function() vim.cmd('vertical resize +15') end)
+vim.keymap.set('n', '<leader>H', function() vim.cmd('vertical resize +15') end)
 vim.keymap.set('n', '<leader>v', function() vim.cmd('vnew') end)
