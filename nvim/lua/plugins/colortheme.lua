b1ded8b737efaa02884ffb334f98f08a8426a719
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-07-29 13:18:00 -0400

    nice

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,18 +1,18 @@
--- return {
---     'Mofiqul/dracula.nvim',
---     opts = {
---         colors = {
---             visual = '#434b8f'
---         },
---     },
---     config = function(_, opts)
---         local dracula = require('dracula')
---         dracula.setup(opts)
---         vim.cmd('colorscheme dracula-soft')
---     end,
---     lazy = false
--- }
-return { 'navarasu/onedark.nvim', opts = {}, config = function() require('onedark').load() end, lazy = false }
+return {
+    'Mofiqul/dracula.nvim',
+    opts = {
+        colors = {
+            visual = '#434b8f'
+        },
+    },
+    config = function(_, opts)
+        local dracula = require('dracula')
+        dracula.setup(opts)
+        vim.cmd('colorscheme dracula-soft')
+    end,
+    lazy = false
+}
+-- return { 'navarasu/onedark.nvim', opts = {}, config = function() require('onedark').load() end, lazy = false }
 -- return { 'rose-pine/neovim', name = 'rose-pine', config = function() vim.cmd('colorscheme rose-pine') end, lazy = false }
 -- return {
 --     'ribru17/bamboo.nvim',
@@ -33,3 +33,13 @@ return { 'navarasu/onedark.nvim', opts = {}, config = function() require('onedar
 -- return { 'catppuccin/nvim', name = 'catppuccin', config = function() vim.cmd('colorscheme catppuccin') end, lazy = false }
 -- return {}
 --
+-- return {
+--     'scottmckendry/cyberdream.nvim',
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.cmd('colorscheme cyberdream')
+--     end
+-- }
+
+-- return { 'rebelot/kanagawa.nvim', lazy = false, priority = 1000, config = function() vim.cmd('colorscheme kanagawa') end }
