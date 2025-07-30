0a2ac87f0671cbcc4533ccffd566a3c884936168
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-07-30 09:26:42 -0400

    markdown plugin added back

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,7 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+    opts = {
+        code = { highlight_border = false, language_border = ' ' },
+    },
+}
