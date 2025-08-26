4165f4c30b938b7534b49bd199072ac98a340e27
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-08-26 14:09:05 -0400

    nice!

--- nvim/lua/config/general/highlights.lua
+++ nvim/lua/config/general/highlights.lua
@@ -1,3 +1,4 @@
 vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#baa0e8', })
 vim.api.nvim_set_hl(0, 'ColorColumn', { fg = '#a787de' })
 vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', { bg = '#5a4480', fg = 'white' })
+vim.api.nvim_set_hl(0, 'SnippetTabstop', {})
