224945a587410f5fb34cf6452c441735bb41ffca
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-10-01 11:51:40 -0400

    ok fine

--- nvim/lua/config/autocmds/lint.lua
+++ nvim/lua/config/autocmds/lint.lua
@@ -1,5 +1,29 @@
-vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+local uv = vim.loop
+
+local eslint_configs = {
+    '.eslintrc.js',
+    '.eslintrc.cjs',
+    '.eslintrc.json',
+    '.eslintrc.yml',
+    '.eslintrc.yaml',
+    'eslint.config.js',
+    'eslint.config.cjs',
+}
+
+local function has_eslint_config(root)
+    for _, fname in ipairs(eslint_configs) do
+        if uv.fs_stat(root .. '/' .. fname) then
+            return true
+        end
+    end
+    return false
+end
+
+vim.api.nvim_create_autocmd('BufWritePost', {
     callback = function()
-        require('lint').try_lint()
+        local root = (vim.lsp.get_active_clients({ bufnr = 0 })[1] or {}).config.root_dir
+        if root and has_eslint_config(root) then
+            require('lint').try_lint()
+        end
     end,
 })
