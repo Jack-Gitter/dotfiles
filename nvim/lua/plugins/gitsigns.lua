96ab93285b1019a6afe21ac4479e235a0e1eae34
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-10-16 08:50:37 -0400

    ok

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,29 +1,17 @@
-return {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-        preview_config = {
-            col = 1,
-            relative = 'cursor',
-            row = 0,
-            style = 'minimal',
-            border = 'single'
-        }
-    },
-    config = function(_, opts)
-        local gitsigns = require('gitsigns')
-        gitsigns.setup(opts)
-        vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk)
-        vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk)
-        vim.keymap.set('n', '<leader>bl', gitsigns.blame)
-        vim.api.nvim_create_autocmd('FileType', {
-            pattern = 'gitsigns-blame',
-            callback = function()
-                vim.keymap.set('n', '<c-c>', ':q<cr>', { buffer = true })
-            end
-        })
-    end,
-    cond = function()
-        return vim.fn.isdirectory('.git') == 1 or
-            vim.fn.system('git rev-parse --is-inside-work-tree 2>/dev/null') == 'true\n'
-    end,
-}
+-- This plugin makes neovim SUUUUUPER slow to quit... not sure why but its so annoying... keeping disabled for now
+return {}
+-- return {
+--     'lewis6991/gitsigns.nvim',
+--     opts = {
+--         current_line_blame = false,
+--         word_diff = false,
+--         attach_to_untracked = false,
+--         signs = { untracked = { enable = false } },
+--         numhl = false,
+--         linehl = false,
+--         max_file_length = 10000,
+--         update_debounce = 300,
+--         watch_gitdir = { interval = 3000, follow_files = true },
+--         yadm = { enable = false },
+--     }
+-- }
