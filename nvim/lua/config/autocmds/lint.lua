69cc7c78e91a65960634371b322c7ce902a37747
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-10-16 09:37:03 -0400

    ok

--- nvim/lua/config/autocmds/lint.lua
+++ nvim/lua/config/autocmds/lint.lua
@@ -1,30 +1,31 @@
-local uv = vim.uv or vim.loop -- use vim.uv in newer Neovim
-
-local eslint_configs = {
-    '.eslintrc.js',
-    '.eslintrc.cjs',
-    '.eslintrc.json',
-    '.eslintrc.yml',
-    '.eslintrc.yaml',
-    'eslint.config.js',
-    'eslint.config.cjs',
-}
-
-local function has_eslint_config(root)
-    for _, fname in ipairs(eslint_configs) do
-        if uv.fs_stat(root .. '/' .. fname) then
-            return true
-        end
-    end
-    return false
-end
-
-vim.api.nvim_create_autocmd('BufWritePost', {
-    callback = function()
-        local clients = vim.lsp.get_clients({ bufnr = 0 })
-        local root = clients[1] and clients[1].config.root_dir
-        if root and has_eslint_config(root) then
-            require('lint').try_lint()
-        end
-    end,
-})
+-- To get lint diagnostics... we don't really need this since we have format on save
+-- local uv = vim.uv or vim.loop
+--
+-- local eslint_configs = {
+--     '.eslintrc.js',
+--     '.eslintrc.cjs',
+--     '.eslintrc.json',
+--     '.eslintrc.yml',
+--     '.eslintrc.yaml',
+--     'eslint.config.js',
+--     'eslint.config.cjs',
+-- }
+--
+-- local function has_eslint_config(root)
+--     for _, fname in ipairs(eslint_configs) do
+--         if uv.fs_stat(root .. '/' .. fname) then
+--             return true
+--         end
+--     end
+--     return false
+-- end
+--
+-- vim.api.nvim_create_autocmd('BufWritePost', {
+--     callback = function()
+--         local clients = vim.lsp.get_clients({ bufnr = 0 })
+--         local root = clients[1] and clients[1].config.root_dir
+--         if root and has_eslint_config(root) then
+--             require('lint').try_lint()
+--         end
+--     end,
+-- })
