48270f9272244ef2a26c627744e5e44fc21d1d96
Jack Gitter <jack.gitter@gmail.com>
2025-10-17 10:33:31 -0400

    committing changes

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,17 +1,15 @@
--- This plugin makes neovim SUUUUUPER slow to quit... not sure why but its so annoying... keeping disabled for now
-return {}
--- return {
---     'lewis6991/gitsigns.nvim',
---     opts = {
---         current_line_blame = false,
---         word_diff = false,
---         attach_to_untracked = false,
---         signs = { untracked = { enable = false } },
---         numhl = false,
---         linehl = false,
---         max_file_length = 10000,
---         update_debounce = 300,
---         watch_gitdir = { interval = 3000, follow_files = true },
---         yadm = { enable = false },
---     }
--- }
+return {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+        current_line_blame = false,
+        word_diff = false,
+        attach_to_untracked = false,
+        signs = { untracked = { enable = false } },
+        numhl = false,
+        linehl = false,
+        max_file_length = 10000,
+        update_debounce = 300,
+        watch_gitdir = { interval = 3000, follow_files = true },
+        yadm = { enable = false },
+    }
+}
