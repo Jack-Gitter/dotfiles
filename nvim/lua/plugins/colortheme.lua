4e4f41192b8ce7cda495061a89c88dc79a17d8ce
Jack Gitter <jack.gitter@gmail.com>
2025-10-17 14:12:16 -0400

    added new colortheme

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,17 +1,17 @@
-return {
-    'Mofiqul/dracula.nvim',
-    opts = {
-        colors = {
-            visual = '#434b8f'
-        },
-    },
-    config = function(_, opts)
-        local dracula = require('dracula')
-        dracula.setup(opts)
-        vim.cmd('colorscheme dracula-soft')
-    end,
-    lazy = false
-}
+-- return {
+--     'Mofiqul/dracula.nvim',
+--     opts = {
+--         colors = {
+--             visual = '#434b8f'
+--         },
+--     },
+--     config = function(_, opts)
+--         local dracula = require('dracula')
+--         dracula.setup(opts)
+--         vim.cmd('colorscheme dracula-soft')
+--     end,
+--     lazy = false
+-- }
 -- return { 'navarasu/onedark.nvim', opts = {}, config = function() require('onedark').load() end, lazy = false }
 -- return { 'rose-pine/neovim', name = 'rose-pine', config = function() vim.cmd('colorscheme rose-pine') end, lazy = false }
 -- return {
@@ -43,3 +43,11 @@ return {
 -- }
 
 -- return { 'rebelot/kanagawa.nvim', lazy = false, priority = 1000, config = function() vim.cmd('colorscheme kanagawa') end }
+return {
+    'everviolet/nvim',
+    name = 'evergarden',
+    priority = 1000,
+    config = function()
+        vim.cmd('colorscheme evergarden-spring')
+    end
+}
