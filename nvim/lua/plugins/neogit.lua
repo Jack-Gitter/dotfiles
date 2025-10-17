fe9b9bd0a5321d1b754e94972ef66f527743c993
Jack Gitter <jack.gitter@gmail.com>
2025-10-17 11:24:32 -0400

    ok

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -18,7 +18,13 @@ return {
             pattern = 'NeogitBranchCreated',
             callback = function()
                 vim.schedule(function()
-                    vim.cmd('Neogit close')
+                    for _, win in ipairs(vim.api.nvim_list_wins()) do
+                        local buf = vim.api.nvim_win_get_buf(win)
+                        local ft = vim.api.nvim_get_option_value('filetype', { buf = buf })
+                        if ft == 'neogitStatus' then
+                            vim.api.nvim_win_close(win, true)
+                        end
+                    end
                 end)
             end,
         })
