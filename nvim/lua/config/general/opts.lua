1add04506c4c59968d116ff95a28c1adc26e5f98
Jack Gitter <jack.gitter@gmail.com>
2025-10-24 14:58:24 -0400

    laundry

--- nvim/lua/config/general/opts.lua
+++ nvim/lua/config/general/opts.lua
@@ -1,3 +1,4 @@
+vim.o.number = true
 vim.o.relativenumber = true
 vim.o.hlsearch = false
 vim.o.shiftwidth = 4
@@ -5,3 +6,7 @@ vim.o.softtabstop = 4
 vim.o.tabstop = 4
 vim.o.winborder = 'rounded'
 vim.o.clipboard = 'unnamedplus'
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.swapfile = false
+vim.o.autoread = true
