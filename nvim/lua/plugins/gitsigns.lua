54a8df0f6bca004dc0b8b5aacf32033b2eb4472f
Jack Gitter <jack.gitter@gmail.com>
2025-10-24 12:22:00 -0400

    ok

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,12 @@
+return {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+	on_attach = function(bufnr)
+	    local gitsigns = require('gitsigns')
+	    vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk, {buffer = bufnr})
+	    vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, {buffer = bufnr})
+	    vim.keymap.set('n', '<leader>bl', gitsigns.blame, {buffer = bufnr})
+	end
+    }
+}
+
