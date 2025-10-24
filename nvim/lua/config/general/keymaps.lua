5ee446b9a5336697ef33ced5458c7f63665975bb
Jack Gitter <jack.gitter@gmail.com>
2025-10-24 15:05:24 -0400

    ok

--- nvim/lua/config/general/keymaps.lua
+++ nvim/lua/config/general/keymaps.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = ' '
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('i', 'kj', '<esc>')
 vim.keymap.set('n', '<c-p>', '<c-^>')
+
+vim.keymap.set('n', '<c-d>', '<c-d>zz')
+vim.keymap.set('n', '<c-u>', '<c-u>zz')
