9bbc13288f4619ded02831f1c9682e1c84cca264
Jack Gitter <jack.gitter@gmail.com>
2025-10-24 12:27:34 -0400

    conform nvim

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+    'stevearc/conform.nvim',
+    opts = {
+        format_on_save = {
+            timeout_ms = 500,
+        },
+        formatters_by_ft = {
+            javascript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
+            typescript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
+            javascriptreact = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
+            typescriptreact = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
+            http = { 'kulala' },
+        },
+    },
+}
