911fa53e2a179b1eada3c5112545507d63bd4cf5
Jack Gitter <jack.gitter@gmail.com>
2025-10-24 08:18:19 -0400

    nice

--- nvim/lua/plugins/leap.lua
+++
@@ -1,9 +0,0 @@
-return {}
--- return {
---     'ggandor/leap.nvim',
---     config = function()
---         vim.keymap.set({ 'n', 'v' }, 'f', '<plug>(leap-forward)')
---         vim.keymap.set({ 'n', 'v' }, '<s-f>', '<plug>(leap-backward)')
---     end,
---     keys = { 'f', 'F' }
--- }
