a74eeb66d927cf290070fa24d818ad73dd459395
Jack Gitter <jack.gitter@gmail.com>
2025-10-26 20:13:15 -0400

    nice

--- nvim/lua/config/general/keymaps.lua
+++ nvim/lua/config/general/keymaps.lua
@@ -5,3 +5,7 @@ vim.keymap.set('n', '<c-p>', '<c-^>')
 
 vim.keymap.set('n', '<c-d>', '<c-d>zz')
 vim.keymap.set('n', '<c-u>', '<c-u>zz')
+
+vim.keymap.set('n', '<leader>cn', function() pcall(vim.cmd.cnext) end, { silent = true })
+vim.keymap.set('n', '<leader>cp', function() pcall(vim.cmd.cprev) end, { silent = true })
+vim.keymap.set('n', '<leader>cq', function() vim.cmd('cclose') end)
