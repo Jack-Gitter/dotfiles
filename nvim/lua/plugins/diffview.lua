43d25534db5c735f4cb7886a2dc3c6869cb4f528
Jack Gitter <jack.gitter@gmail.com>
2025-10-31 15:36:54 -0400

    test

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,4 +1,11 @@
 local diffview = require('diffview')
-diffview.setup()
+diffview.setup({
+	view = {
+		merge_tool = {
+			layout = 'diff3_mixed',
+			disable_diagnostics = true,
+		},
+	},
+})
 vim.keymap.set('n', '<leader>diff', function() vim.cmd('DiffviewOpen') end)
 vim.keymap.set('n', '<c-c>', function() vim.cmd('DiffviewClose') end)
