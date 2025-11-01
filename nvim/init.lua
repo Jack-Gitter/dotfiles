dd377ff2845e9664c0e581e475ad0d2e4dbdb1f7
Jack Gitter <jack.gitter@gmail.com>
2025-11-01 15:41:48 -0400

    ok

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,2 @@
-vim.opt.runtimepath:append(vim.fn.stdpath('config') .. '/' .. 'lsp')
-
 require('config')
 require('plugins')
-require('lsp')
--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,3 +1,4 @@
+require('config.lsp')
 require('config.keymaps')
 require('config.autocmds')
 require('config.opts')
