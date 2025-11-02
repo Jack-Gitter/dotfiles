260d0bef2a8746b030fdf0e74c45be3baed9e596
Jack Gitter <jack.gitter@gmail.com>
2025-11-02 07:00:40 -0500

    ok

--- nvim/lsp/lua.lua
+++ nvim/lsp/lua.lua
@@ -27,7 +27,7 @@ return {
 			workspace = {
 				checkThirdParty = 'Disable',
 				ignoreDir = { '~/.local/' },
-				library = vim.api.nvim_get_runtime_file('', true)
+				library = vim.api.nvim_get_runtime_file('lua', true)
 			},
 			diagnostics = {
 				libraryFiles = 'Disable',
