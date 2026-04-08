db886b49e360b414cf2b90b775dee7b7c858723b
Jack Gitter <jack.gitter@sentinelgroup.com>
2026-04-08 08:15:32 -0400

    ok

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -2,9 +2,6 @@ local codecompanion = require('codecompanion')
 
 codecompanion.setup({
 	interactions = {
-		chat = {
-			adapter = 'anthropic',
-		},
 		cli = {
 			agent = 'claude_code',
 			agents = {
@@ -15,17 +12,6 @@ codecompanion.setup({
 					provider = 'terminal',
 				},
 			},
-		},
-	},
-	adapters = {
-		http = {
-			anthropic = function()
-				return require('codecompanion.adapters').extend('anthropic', {
-					env = {
-						api_key = 'ANTHROPIC_API_KEY',
-					},
-				})
-			end,
 		},
 	},
 })
