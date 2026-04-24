e74db7cc5fb0263c83c53f3b182d5689baf61d53
Jack Gitter <jack.gitter@sentinelgroup.com>
2026-04-24 15:27:59 -0400

    pleaseeeeee

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 vim.pack.add({
-	{ src = 'https://github.com/rose-pine/neovim',                name = 'rosepine' },
+	-- { src = 'https://github.com/rose-pine/neovim',                name = 'rosepine' },
+	{ src = 'https://github.com/sainnhe/gruvbox-material',        name = 'gruvbox' },
 	{ src = 'https://github.com/stevearc/conform.nvim',           name = 'conform.nvim' },
 	{ src = 'https://github.com/lewis6991/gitsigns.nvim',         name = 'gitsigns.nvim' },
 	{ src = 'https://github.com/mason-org/mason.nvim',            name = 'mason.nvim' },
