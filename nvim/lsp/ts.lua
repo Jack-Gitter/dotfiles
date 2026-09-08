4889c7dcc8eb9e101d529b795e2a635bdbf29106
Jack Gitter <jack.gitter@sentinelgroup.com>
2026-09-08 08:55:54 -0400

    use tsgo

--- nvim/lsp/ts.lua
+++ nvim/lsp/ts.lua
@@ -19,3 +19,25 @@ return {
 		}
 	},
 }
+--
+-- return {
+-- 	init_options = { hostInfo = 'neovim', },
+-- 	cmd = { 'tsgo', '--lsp', '--stdio' },
+-- 	filetypes = {
+-- 		'javascript',
+-- 		'javascriptreact',
+-- 		'javascript.jsx',
+-- 		'typescript',
+-- 		'typescriptreact',
+-- 		'typescript.tsx',
+-- 	},
+-- 	root_markers = {
+-- 		'tsconfig.json', 'jsconfig.json', 'package.json', '.git'
+-- 	},
+-- 	single_file_support = true,
+-- 	settings = {
+-- 		completions = {
+-- 			completeFunctionCalls = true
+-- 		}
+-- 	},
+-- }
